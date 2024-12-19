630240253735cef219f2835fc9c24628ba3975d8
hanaru <133945249+1roo@users.noreply.github.com>
2024-12-19 14:21:08 +0900

    241219 header done

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const UserController = require("../controller/CUser");
 const router = express.Router();
 
 router.get("/", UserController.main);
+router.get("/signup", UserController.signup);
 
 module.exports = router;
